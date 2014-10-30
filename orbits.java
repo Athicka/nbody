24a8f51ee340c0ae39617e12500cad3b0f82684b
Athicka <Athicka@users.noreply.github.com>
2014-10-30 14:10:11 -0800

    Update orbits.java

--- orbits.java
+++ orbits.java
@@ -1,4 +1,4 @@
-	import javax.swing.JFrame;
+import javax.swing.JFrame;
 
 	import org.math.plot.*;
 
@@ -24,24 +24,26 @@ public class orbits {
 
 }
 	void round(){
-		//still dosnt work out bounds
+		
 	xpos[0]=1.0;
 	ypos[0]=0.0;
-	xvel[0]=0.0;
-	yvel[0]=0.0;
-	double dt=0.001;
+	xvel[0]=2.0;
+	yvel[0]=2.0;
+	double dt=0.01;
 	double r=1;
 	int loc= 1;
-
-	for(double i=.001 ; i<1000 ; i=i+.001){
-	if(i==.001)		r=Math.sqrt(xpos[loc]*xpos[loc] + (ypos[loc]*ypos[loc]));
 	
-	xvel[loc]=xvel [loc-1]-((4*pi*xpos[loc-1]*.001)/(r*r*r));
-	yvel[loc]=yvel [loc-1]-((4*pi*ypos[loc-1]*.001)/(r*r*r));
+	for(double i=.01 ; i<1 ; i=i+.01){
+	System.out.println(xpos[loc-1]);
+	System.out.println(ypos[loc-1]);
+	r=Math.sqrt((xpos[loc-1]*xpos[loc-1]) + (ypos[loc-1]*ypos[loc-1])); 
+	
+	xvel[loc]=xvel [loc-1]-((4*pi*xpos[loc-1]*dt)/(r*r*r));   
+	yvel[loc]=yvel [loc-1]-((4*pi*ypos[loc-1]*dt)/(r*r*r));
 	
 	
-	xpos[loc]=xpos[i-1]+xvel[loc]*dt;				//fixed x position
-	ypos[loc]=ypos[i-1]+yvel[loc]*dt;				//fixed y pos
+	xpos[loc]=xpos[loc-1]+xvel[loc]*dt;				//fixed x pos
+	ypos[loc]=ypos[loc-1]+yvel[loc]*dt;				//fixed y pos
 	
 	loc++;
 	
@@ -50,17 +52,22 @@ public class orbits {
 	}
 	
 	private void graph() {
-		 //untested
+		  
 		  // create your PlotPanel (you can use it as a JPanel)
 		  Plot2DPanel plot = new Plot2DPanel();
 		 
 		  // add a line plot to the PlotPanel
 		  plot.addLinePlot("my plot", xpos, ypos);
-		 
+		  //why is it graphing the first point at (0,0) not (1,0)
+		  
 		  // put the PlotPanel in a JFrame, as a JPanel
 		  JFrame frame = new JFrame("a plot panel");
+		 
 		  frame.setContentPane(plot);
 		  frame.setVisible(true);
+		  //makes it appear semi centered
+		  frame.setLocation(250, 250);
+		  frame.setSize(600, 600);
 		
 	}
 
